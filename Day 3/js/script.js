3dfd95a0f3f99ef17e182649ab5051a7b4602a42
1coderbaba <148226464+1coderbaba@users.noreply.github.com>
2023-10-18 15:50:11 +0530

    day 3

---
+++ Day 3/js/script.js
@@ -0,0 +1,27 @@
+// console.log("hello")
+// console.error(" this IS ERRIOR")
+// console.warn("this is warning")
+
+// let age = prompt("enter value of age")
+
+// if(age<18)
+// {
+//     console.log("can't vote")
+// }
+// else
+// {
+//     console.log("can vote")
+// }
+
+// for(let i=0;i<10;i++)
+// {
+//     console.log(i)
+// }
+
+// console.log(Math.PI)
+// console.log(Math.log2(8))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(4.7))
+// console.log(Math.round(4.7))
+
+console.log(Math.random()*10)
